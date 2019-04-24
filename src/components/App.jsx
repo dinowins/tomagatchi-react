8b50d1478ddfa2bcee1934a97a9890b306b3bbd0
Dino Nguyen and Nate Wessels <dino@>
2019-04-24 15:59:09 -0700

    passing state down to child

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 import Main from './Main';
 import Error404 from './Error404';
 
-function App(){
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route component={Main} path='/' />
-        <Route component={Error404}/>
-      </Switch>
-      <h1 style={{height: '50px'}}></h1>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      main: {
+        food: 100,
+        mood: 100,
+        sleep: 100
+      }
+    }
+    this.decrease = this.decrease.bind(this);
+
+  }
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() => {
+      this.decrease();
+
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  decrease() {
+    let newFood = Object.assign({}, this.state.main.food);
+    let mood = this.state.main.mood - 0.5;
+    let sleep = this.state.main.sleep - 0.5;
+    let food = this.state.main.food - 0.5;
+    console.log(mood);
+    this.setState({
+      main: {
+        mood: mood,
+        food: food,
+        sleep: sleep
+      }
+    })
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route path='/' render={()=><Main main={this.state.main} />} />
+          <Route component={Error404}/>
+        </Switch>
+        <h1 style={{height: '50px'}}></h1>
+      </div>
+    );
+
+  }
 }
 
 export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,132 +1,19 @@
-import React, {Component} from 'react';
-import Stats from './Stats';
+import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
 
+function Main(props) {
+  console.log(props.main);
+  return (
+    <div>{props.main.food}</div>
 
-class Main extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      name: PropTypes.string,
-      foodLevel: PropTypes.number,
-      playLevel: PropTypes.number,
-      sleepLevel: PropTypes.number
-    };
-    console.log(this.state);
-  }
-
-  startLevels() {
-    this.state.name = 'George';
-    this.state.foodLevel = 100;
-    this.state.playLevel = 100;
-    this.state.sleepLevel = 100;
-  }
-  check() {
-    let that = this;
-    setInterval(function() {
-      if(that.state.foodLevel == 0 || that.state.playLevel == 0 || that.state.sleepLevel == 0) {
-        clearInterval();
-        console.log("Is Dead");
-      }
-    }, 3000);
-  }
-
-
-  eat(){
-    console.log(this.state);
-    let newfoodLevel = this.state.foodLevel + 5;
-    this.setState({
-      foodLevel: newfoodLevel
-    });
-    console.log(this.state.foodLevel)
-  }
-
-  sleep() {
-
-  }
-  play() {
-
-  }
-  power() {
-
-  }
-
-  render() {
-    let that = this;
-    this.startLevels();
-    this.check();
-
-    let statsStyle ={
-      zIndex: '-1',
-    }
-
-    let body = {
-      margin: '0 auto',
-      display: 'flex',
-      justifyContent: 'center',
-      // alignItems: 'center'
-    }
-
-    let image = {
-      position: 'absolute',
-      zIndex: '-1',
-      display: 'flex',
-      justifyContent: 'center',
-      // alignItems: 'center',
-    }
-    let ninja = {
-      width: '150px',
-      height: '150px',
-      display: 'flex',
-      justifyContent: 'center',
-      marginLeft: '25px',
-      marginTop: '15px'
-    }
-
-    let main = {
-      width: '200px',
-      height: '300px',
-      margin: '0 auto'
-    }
-
-    let buttons = {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      width: '215px',
-      marginRight: '100px',
-
-    }
-
-    let button = {
-      marginTop: '85px'
-    }
-
-
-
-
-    return (
-      <div style={main}>
-        <div style={body}>
-          <img src={require('../assets/images/Main.png')} alt='' style={image} />
-          <Stats state={this.state} style={statsStyle}/>
-        </div>
-        <img src={require('../assets/images/ninja.png')} alt='' style={ninja} />
-        <div style={buttons}>
-          <button style={button} onClick={() => {that.eat()}}></button>
-          <button style={button} onClick={that.sleep}></button>
-          <button style={button} onClick={that.play}></button>
-          <button style={button} onClick={that.power}></button>
-
-        </div>
-    </div>
-
-    );
-  }
+  );
 }
-
+// Main.propTypes = {
+//   decreaseFood: PropTypes.func,
+//   decreaseSleep: PropTypes.func,
+//   decreaseMood: PropTypes.func
+// }
 
 
 export default Main;
