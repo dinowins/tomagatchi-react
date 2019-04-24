686e1bda35fd977a971e631db38fff074121b236
Dino Nguyen and Nate Wessels <dino@>
2019-04-24 16:22:19 -0700

    Add mood, food, sleep change on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,11 +30,34 @@ class App extends React.Component{
     clearInterval(this.waitTimeUpdateTimer);
   }
 
+  add(increase) {
+    let mood = this.state.main.mood;
+    let sleep = this.state.main.sleep;
+    let food = this.state.main.food;
+    if(increase == "mood") {
+      mood = mood + 5;
+    } else if(increase == "sleep") {
+      sleep = sleep + 5;
+    } else if(increase == "food") {
+      food = food + 5;
+    }
+    this.setState({
+      main: {
+        mood: mood,
+        food: food,
+        sleep: sleep
+      }
+    })
+  }
+
   decrease() {
     let newFood = Object.assign({}, this.state.main.food);
     let mood = this.state.main.mood - 0.5;
     let sleep = this.state.main.sleep - 0.5;
     let food = this.state.main.food - 0.5;
+    if(this.state.main.food <= 0 || this.state.main.mood <= 0 || this.state.main.sleep <= 0) {
+      clearInterval(this.waitTimeUpdateTimer);
+    }
     console.log(mood);
     this.setState({
       main: {
@@ -57,6 +80,9 @@ class App extends React.Component{
           <Route component={Error404}/>
         </Switch>
         <h1 style={{height: '50px'}}></h1>
+        <button onClick={() => {this.add('food')}}>Food</button>
+        <button onClick={() => {this.add('sleep')}}>Sleep</button>
+        <button onClick={() => {this.add('mood')}}>Mood</button>
       </div>
     );
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
 
-function Main(props) {
-  console.log(props.main);
-  return (
-    <div>{props.main.food}</div>
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
 
+
+
+
+  render() {
+    return (
+      <div><h2>Food: {this.props.main.food}</h2><h2>Sleep: {this.props.main.sleep}</h2><h2>Mood: {this.props.main.mood}</h2>
+
+
+
+    </div>
   );
+  }
 }
 // Main.propTypes = {
 //   decreaseFood: PropTypes.func,
