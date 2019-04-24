bc446723c53054a7b5a234523e7f7219042b3896
Dino Nguyen and Nate Wessels <nate@>
2019-04-24 16:43:00 -0700

    styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,14 +8,76 @@ class Main extends React.Component {
     this.props = props;
   }
 
-
-
-
   render() {
+
+    let body = {
+      margin: '0 auto',
+      width: '600px',
+      height: '600px',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column'
+
+    }
+
+    let image = {
+      width: '400px',
+      position: 'absolute',
+      zIndex: '-1'
+
+    }
+
+    let stats = {
+      width: '200px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      height: '10px',
+
+    }
+
+    let statNames = {
+      width: '200px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    }
+
+    let allStats = {
+      marginLeft: '10px',
+      marginTop: '60px'
+    }
+
+    let h2 = {
+      height: '10px'
+    }
+
+    let ninja = {
+      marginTop: '40px',
+      width: '200px',
+      height: '200px'
+    }
+
+
+
+
     return (
-      <div><h2>Food: {this.props.main.food}</h2><h2>Sleep: {this.props.main.sleep}</h2><h2>Mood: {this.props.main.mood}</h2>
+      <div style={body}>
+        <img src={require('../assets/images/Main.png')} style={image}></img>
+        <div style={allStats}>
+          <div style={statNames}>
+            <h2 style={h2}>Food</h2>
+            <h2 style={h2}>Sleep</h2>
+            <h2 style={h2}>Mood</h2>
+          </div>
+          <div style={stats}>
+            <h2 style={h2}> {this.props.main.food}</h2>
+            <h2 style={h2}>{this.props.main.sleep}</h2>
+            <h2 style={h2}>{this.props.main.mood}</h2>
+          </div>
+        </div>
 
-
+        <img src={require('../assets/images/ninja.png')} style={ninja}/>
 
     </div>
   );
